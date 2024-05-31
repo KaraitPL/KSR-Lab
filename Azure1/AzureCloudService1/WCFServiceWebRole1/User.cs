94d3438b148dfd1a7da7d2d11274e7c44897e8d2
KaraitPL <karaitpl@gmail.com>
2024-05-31 23:52:52 +0200

    Added Azure Lab1

---
+++ Azure1/AzureCloudService1/WCFServiceWebRole1/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace WCFServiceWebRole1
+{
+    public class User : TableEntity
+    {
+        public User(string rk, string pk)
+        {
+            this.PartitionKey = pk; // ustawiamy klucz partycji
+            this.RowKey = rk; // ustawiamy klucz główny
+                                       // this.Timestamp; jest tylko do odczytu
+        }
+        public User() { }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Guid SessionId { get; set; }
+    }
+}
